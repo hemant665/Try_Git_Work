a769caddae4c989cd50ca5d30bf0fa3b4e8615b0
Hemant Kumar <hemant04kumawat@gmail.com>
2025-06-28 14:54:02 +0530

    kljshd

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>New Blaster Branch</h1>
+      <p>this is the new commit in the blaster branch</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
